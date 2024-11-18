407c3cb04ff3d17bfc16aee06814c6f06972c300
AKROMADOKSA\idiaz <idiazmiragaya294@gmail.com>
2024-11-18 14:59:31 +0100

    better localstorage for saving data?

--- player_data_Script.js
+++ player_data_Script.js
@@ -5,20 +5,11 @@ const path = require('path');
 const saveDataPath = path.join(__dirname, '../data/data.json');
 
 function guardarDatos(data) {
-    fs.writeFileSync(saveDataPath, JSON.stringify(data), 'utf8');
-    console.log("Datos guardados en save_data.json");
+    localStorage.setItem('data', JSON.stringify(data));
 }
 
 function leerDatos() {
-    try {
-        const data = fs.readFileSync(saveDataPath, 'utf8');
-        const miVariableRecuperada = JSON.parse(data);
-        console.log("Datos leídos desde save_data.json:", miVariableRecuperada);
-        return miVariableRecuperada;
-    } catch (error) {
-        console.error("Error al leer los datos:", error);
-        return null;
-    }
+    return localStorage.getItem('data');
 }
 
 /*
