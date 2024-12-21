f9586c3b3f1b88ba43a9ad7d9d785d2abdecf890
AKROMADOKSA\idiaz <idiazmiragaya294@gmail.com>
2024-12-21 05:02:35 +0100

    some tests

--- data/global_variables.js
+++ data/global_variables.js
@@ -14,11 +14,11 @@ let dados = ["11", "12", "13", "14", "15", "16", "22", "23", "24", "25", "26", "
 
 //dados = ["00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e","00e"]
 
-let objetos = ["luck","demon","coin","blank","lastBurn","mirror","magnetic","tedTalk"] //todos los objetos disponibles
+//let objetos = ["luck","demon","coin","blank","lastBurn","mirror","magnetic","tedTalk"] //todos los objetos disponibles
 //objetos = ["mirror","mirror","mirror","mirror"] //todos los objetos disponibles
 //objetos = ["luck","luck","luck","luck"] //todos los objetos disponibles
 //objetos = ["demon","demon","demon","demon"] //todos los objetos disponibles
-objetos = ["coin","coin","coin","coin"] //todos los objetos disponibles
+//objetos = ["coin","coin","coin","coin"] //todos los objetos disponibles
 //objetos = ["blank","blank","blank","blank"] //todos los objetos disponibles
 //objetos = ["lastBurn","lastBurn","lastBurn","lastBurn"] //todos los objetos disponibles
 //objetos = ["magnetic","magnetic","magnetic","magnetic"] //todos los objetos disponibles
